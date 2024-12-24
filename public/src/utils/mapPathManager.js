05e2055b181a68cd764eaaa00a860334da7c6441
Jaeyoung <jyko1101@gmail.com>
2024-12-24 17:04:35 +0900

    refactor: 기획변경된 path generation

--- public/src/utils/mapPathManager.js
+++ public/src/utils/mapPathManager.js
@@ -5,12 +5,12 @@
 //====================================================================================================================
 //====================================================================================================================
 
+/* LEGACY CODE : 게임모드에 따라 다른 몬스터 경로 생성 << 모드 상관없이 단일 맵 기획변경
 // 임시 게임모드 enum 역할
 export const GameMode = Object.freeze({
     SINGLE: 'single',
     DUO: 'duo'
 });
-
 // 게임모드에 따라 몬스터 경로 생성
 export const generateMonsterPath = (gameMode) => {
     switch (gameMode) {
@@ -22,6 +22,7 @@ export const generateMonsterPath = (gameMode) => {
             break;
     }
 };
+*/
 
 //
 export const generateSingleModePath = () => {
@@ -29,19 +30,29 @@ export const generateSingleModePath = () => {
     let currentX = 0;
     let currentY = Math.floor(Math.random() * 21) + 500; // 500 ~ 520 범위의 y 시작
     path.push({ x: currentX, y: currentY });
-    // 캔버스 위치로 snapping
-    /*
-    while (currentX < canvas.width) {
-        currentX += Math.floor(Math.random() * 100) + 50; // 50 ~ 150 범위의 x 증가
-        if (currentX > canvas.width) currentX = canvas.width;
-
-        currentY += Math.floor(Math.random() * 200) - 100; // -100 ~ 100 범위의 y 변경
-        if (currentY < 0) currentY = 0;
-        if (currentY > canvas.height) currentY = canvas.height;
-
-        path.push({ x: currentX, y: currentY });
+    // 캔버스 위치로 snapping 현재 canvas width="1920" height="1080" 고려, 임시로 하드코딩 >> 추후 config로 설정값 관리 리팩토링 필요
+    const basePoint = { x: 100, y: 100 };
+    const incremWidth = (1920 * (9 / 10)) / 2;
+    const incremHeight = 1080 * (9 / 10);
+    // 순차적으로 가야하는 sequnece
+    const simplifiedSequence = [
+        { x: 0, y: 0 },
+        { x: 1, y: 0 },
+        { x: 1, y: 1 },
+        { x: 2, y: 1 },
+        { x: 2, y: 0 },
+        { x: 1, y: 0 },
+        { x: 1, y: 0 },
+        { x: 1, y: 1 },
+        { x: 0, y: 1 },
+    ];
+    const processedSequence = simplifiedSequence.map(point => ({
+        x: basePoint.x + point.x * incremWidth,
+        y: basePoint.y + point.y * incremHeight
+    }));
+    for (let point of processedSequence) {
+        path.push(point);
     }
-        */
 
     return path;
 }
