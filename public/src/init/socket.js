a0fecb9c4a6e6161365e308c915807873adbf8e7
조용필 <78397393+bbie-6772@users.noreply.github.com>
2024-12-26 15:20:28 +0900

    [add] 서버 접속 수정
    서버에 접속할 때 로그인 여부 판정 및 로그인 정보를 서버에 전달하도록 수정

--- public/src/init/socket.js
+++ public/src/init/socket.js
@@ -1,13 +1,18 @@
 import { CLIENT_VERSION } from "./constants.js";
 
-let userInfo = null
-let rankings = null
+const token = localStorage.getItem("access-Token")
+// 로그인이 안되어있을 시 로그인 창으로
+if (!token) {
+    alert("로그인이 필요한 서비스 입니다!")
+    window.location.href = './login.html';
+}
 
 // localhost:3000 에 서버를 연결하여 값을 넘겨줌
 const socket = io('http://localhost:3000', {
     query: {
         clientVersion: CLIENT_VERSION,
-        // 엑세스 토큰을 줘서 사용자 로그인 여부 확인?
+        // 엑세스 토큰을 줘서 사용자 로그인 여부 확인
+        accessToken: token
     },
 });
 
@@ -18,9 +23,6 @@ socket.on('response', (data) => {
 // 클라이언트에서 총합적으로 server에 보내주는걸 관리
 export const sendEvent = (handlerId, payload) => {
     socket.emit('event', {
-        userId: userInfo.uuid,
-        clientVersion: CLIENT_VERSION,
-        handlerId,
-        payload,
+
     });
 };
