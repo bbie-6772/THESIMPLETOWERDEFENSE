afec56ecdad33af75fa194f27694b3116780f985
조용필 <78397393+bbie-6772@users.noreply.github.com>
2024-12-26 15:19:52 +0900

    [fix] 소켓 연결
    소켓 연결이 되도록 수정

--- src/init/socket.js
+++ src/init/socket.js
@@ -1,4 +1,6 @@
 import { Server as SocketIO } from 'socket.io';
+import registerHandler from '../handlers/register.handler.js';
+
 
 const initSocket = (server) => {
     // 서버 생성
@@ -6,7 +8,7 @@ const initSocket = (server) => {
     // initSocket에 받은 server의 포트와 연결함
     io.attach(server)
     // 핸들러 등록
-    
+    registerHandler(io)
 }
 
 export default initSocket
