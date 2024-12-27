6cbdadf20a133356ba8a1af54dc3b6e47afb8ae8
조용필 <78397393+bbie-6772@users.noreply.github.com>
2024-12-27 20:17:09 +0900

    [fix] socket 입/출력 동기화

--- public/src/init/socket.js
+++ public/src/init/socket.js
@@ -1,11 +1,11 @@
 import { CLIENT_VERSION } from "./constants.js";
-import { updateRooms } from "../../lobby.js"
-import { updateRoomInfo, updateUser } from "../../room.js";
+import { updateRooms, updateRoomInfo, updateUser } from "../../lobby.js"
 
 let userId = null;
 let nickname = null;
 let highScoreS = null;
 let highScoreM = null;
+let room = null;
 
 const token = localStorage.getItem("access-Token")
 // 로그인이 안되어있을 시 로그인 창으로
@@ -26,37 +26,54 @@ const socket = io('http://localhost:3000', {
 socket.once('connection', (data) => {
     if (data.status === "fail") {
         alert("다시 로그인 해주세요!")
-        window.location.href = './login.html';
+        window.location.href = 'login.html';
     } else {
         // 값 저장
-        [ userId, nickname, highScoreS, highScoreM ]= data
+        [ userId, nickname, highScoreS, highScoreM]= data
+        // 방 목록 업데이트
+        updateRooms(data[4])
     }
 });
 
 socket.on("response", (data) => {
-    console.log(data)
-
-    if (data?.status === "fail") return alert(data.message)
-    // 방 입장 핸들러
-    if (data[0] === 1001 ) {
-        // 방 생성 성공 시 room 페이지로 이동
-        window.location.href = "room.html";
-        updateRoomInfo(data[1].room)
-        updateUser(data[1].room)
-    }   
-    // 방 로딩 핸들러
-    if (data[0] === 1002) {
-        updateRooms(data[1].rooms)
-    }
 })
 
 // 클라이언트에서 총합적으로 server에 보내주는걸 관리
-export const sendEvent = (handlerId, payload) => {
-    socket.emit('event', {
-        userId,
-        token,
-        handlerId,
-        clientVersion: CLIENT_VERSION,
-        payload
-    });
+export const sendEvent = async (handlerId, payload) => {
+
+    const log = await new Promise((resolve, reject) => {
+        socket.emit('event', {
+            userId,
+            token,
+            handlerId,
+            clientVersion: CLIENT_VERSION,
+            payload
+        });
+
+        socket.once('response', (data) => {
+            if (data[1]?.status === "fail") {
+                alert(data[1].message)
+                return resolve(false)
+            }
+            // 방 입장 핸들러
+            if (data[0] === 1001) {
+                updateRoomInfo(data[1].room)
+                updateUser(data[1].room)
+            }
+            // 방 로딩 핸들러
+            if (data[0] === 1002) {
+                updateRooms(data[1].rooms)
+            }
+            return resolve(true)
+        })
+
+
+        
+        setTimeout(reject,2000)
+    })
+    return log
 };
+
+export const getRoom = () => {
+    return room
+}
