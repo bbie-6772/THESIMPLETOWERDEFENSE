731b9db097bd980c0fec5a799817048743db6497
조용필 <78397393+bbie-6772@users.noreply.github.com>
2024-12-27 20:44:11 +0900

    [fix] cors 오류 해결
    오리진 문제 수정

--- src/app.js
+++ src/app.js
@@ -10,7 +10,9 @@ const server = createServer(app);
 
 const PORT = 3000;
 
-app.use(cors())
+app.use(cors({
+  origin: "*",
+}))
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static("public"));
