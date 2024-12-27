afc6b87aa87439775d052b20822b95c9eece84ee
Jaeyoung <jyko1101@gmail.com>
2024-12-27 17:45:43 +0900

    fix: swal setting

--- public/src/utils/sweetAlert.js
+++ public/src/utils/sweetAlert.js
@@ -4,9 +4,6 @@
 // js for SWAL2 utility
 //====================================================================================================================
 //====================================================================================================================
-import Swal from 'sweetalert2';
-
-
 // notice: icon 종류 : success, error, warning, info, question
 export function showAlert(title, text, icon = 'info', confirmButtonText = 'OK') {
     Swal.fire({
