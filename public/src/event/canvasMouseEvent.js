f113276cfae9abd02e72f9f934f7549b5cba3269
foreiner <74106693+foreiner@users.noreply.github.com>
2024-12-27 20:22:15 +0900

    update & fix 클라이언트 관련 수정
    마우스 이벤트 추가 및 game에 있던 마우스 이벤트 이동
    버튼 json 파일 생성 및 처리를 위한 assets,js 내용 수정
    버튼 모달 생성
    이후 타워 그리드 관련 내용 필요

---
+++ public/src/event/canvasMouseEvent.js
@@ -0,0 +1,97 @@
+import { GetTowerFromCoordinate } from "../model/tower.js";
+import { getButtons } from "../model/buttons.model.js";
+
+//마우스 이벤트 초기 설정
+export const canvasMouseEventinit = (canvas) => {
+  canvas.addEventListener("click", handleClick);
+  canvas.addEventListener("mousedown", handleMousedown);
+  canvas.addEventListener("mouseup", handleMouseup);
+  canvas.addEventListener("mouseover", handleMouseover);
+  canvas.addEventListener("mousemove", handleMousemove);
+};
+
+//마우스 위치와 아이콘 정보
+let holdingicon = {};
+let isHolding = false;
+let mousePosition = [0, 0];
+
+//캔버스 정보
+const Canvas = document.getElementById("gameCanvas");
+const ctx = Canvas.getContext("2d");
+const canvasRect = Canvas.getBoundingClientRect();
+
+//클릭 함수(디버그 캔버스에 그림 그리는중)
+function handleClick(event) {
+  const debugCanvas = document.getElementById("debugCanvas");
+  const dctx = debugCanvas.getContext("2d");
+  const dcanvasRect = debugCanvas.getBoundingClientRect();
+  const scaleX = debugCanvas.width / dcanvasRect.width; // 가로 스케일
+  const scaleY = debugCanvas.height / dcanvasRect.height; // 세로 스케일
+
+  var x = (event.clientX - dcanvasRect.left) * scaleX;
+  var y = (event.clientY - dcanvasRect.top) * scaleY;
+  var t = GetTowerFromCoordinate(x, y);
+  if (t) {
+    dctx.clearRect(0, 0, debugCanvas.width, debugCanvas.height);
+    dctx.fillStyle = "green";
+    dctx.lineWidth = 3; // 테두리 두께
+    dctx.strokeRect(t.x, t.y, t.width, t.height);
+    console.log(x, y);
+    console.log(t);
+  }
+}
+//마우스 버튼을 누른 경우
+function handleMousedown(event) {
+  const buttons = getButtons();
+  console.log(buttons);
+  for (let i = 0; i < buttons.length; i++) {
+    if (buttons[i].mouseOver == true) {
+      const holdingImage = new Image();
+
+      if (buttons[i].text === "랜덤타워") {
+        holdingImage.src = "../assets/images/questionMark.png";
+        holdingicon = { button: buttons[i], image: holdingImage };
+        isHolding = true;
+        break;
+      } else if (buttons[i].text === "타워판매") {
+        holdingImage.src = "../assets/images/moneyMark.png";
+        holdingicon = { button: buttons[i], image: holdingImage };
+        isHolding = true;
+        break;
+      }
+    }
+  }
+}
+//마우스 버튼을 땐 경우
+function handleMouseup(event) {
+  if ("button" in holdingicon) {
+  }
+  isHolding = false;
+  holdingicon = {};
+}
+//캔버스에 진입한 경우
+function handleMouseover(event) {}
+//캔버스상에서 마우스를 움직인 경우우
+function handleMousemove(event) {
+  const scaleX = Canvas.width / canvasRect.width; // 가로 스케일
+  const scaleY = Canvas.height / canvasRect.height; // 세로 스케일
+
+  mousePosition[0] = (event.clientX - canvasRect.left) * scaleX;
+  mousePosition[1] = (event.clientY - canvasRect.top) * scaleY;
+  const buttons = getButtons();
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].checkMouseOver(mousePosition[0], mousePosition[1]);
+  }
+}
+//게임 루프에서 마우스포인터 위치에 아이콘 붙이는 함수
+export const drawmousePoint = (ctx) => {
+  if (isHolding && "image" in holdingicon) {
+    ctx.drawImage(
+      holdingicon.image,
+      mousePosition[0] - 25,
+      mousePosition[1] - 25,
+      50,
+      50
+    );
+  }
+};
