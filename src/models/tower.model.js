f5c4d1c26ea8fc375e4d00191a7b6f99787bc532
ChanilPark <49317214+Aeroxol@users.noreply.github.com>
2024-12-30 14:44:08 +0900

    [feat] tower upgrade, server handler
    -타워 업그레이드 기능 구현
    -서버측 핸들러 구현
    -합성, 강화시 검증 절차 구현

--- src/models/tower.model.js
+++ src/models/tower.model.js
@@ -1,4 +1,5 @@
 import { getGameAssets } from "../init/assets.js";
+import { getUser } from "./users.model.js";
 
 const towers = [];
 
@@ -15,6 +16,8 @@ const tier_scale_target = 1;
 const tier_scale_range = 1;
 const tier_scale_cost = 1;
 const tier_scale_cooldown = 1;
+export const max_tier = 5;
+export const max_upgrade = 5;
 
 
 //타워 추가
@@ -123,14 +126,20 @@ export const merge = (uuid, x1, y1, x2, y2) => {
   // other 자리에 무작위 상위 티어 타워를 생성한다
   const towers = getGameAssets().towers;
   const randomTower = getRandomInt(0, towers.data.length);
-  addTower(uuid, x, y, randomTower, newtier);
+  addTower(uuid, x2, y2, randomTower, newtier);
 
   // 생성된 타워에 대한 정보를 반환한다(클라이언트에 전달해야함)
   return { randomTower, newtier, x2, y2 };
 }
 
 export const upgrade = (uuid, towerId) => {
-
+  // 유저의 타워 업그레이드 수치를 증가시킨다
+  const user = getUser(uuid);
+  if (!user.upgrade[towerId]) {
+    user.upgrade[towerId] = 0;
+  }
+  user.upgrade[towerId]++;
+  return { uuid: uuid, type: towerId, level: user.upgrade[towerId] };
 }
 
 const towerModel = {
@@ -144,6 +153,8 @@ const towerModel = {
   towerCoolDown,
   merge,
   upgrade,
+  max_tier,
+  max_upgrade,
 };
 
 export default towerModel;
